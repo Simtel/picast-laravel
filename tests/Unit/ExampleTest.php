aca908e79192da5fab8038085eeea3c148ed6f5d
Simtel <simuls@gmail.com>
2024-05-17 11:40:08 +0400

    add db transaction in tests

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -4,21 +4,30 @@ namespace Tests\Unit;
 
 use App\Events\DomainCreated;
 use App\Models\Domain;
+use DB;
 use Event;
-use Illuminate\Foundation\Testing\TestCase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\CreatesApplication;
+use Tests\TestCase;
+use Throwable;
 
 class ExampleTest extends TestCase
 {
     use CreatesApplication;
+    use DatabaseTransactions;
 
+    /**
+     * @throws Throwable
+     */
     public function test_example(): void
     {
+        DB::beginTransaction();
         Event::fake([DomainCreated::class]);
         Domain::factory(1)->create();
 
         $m = Domain::first();
         self::assertInstanceOf(Domain::class, $m);
+        DB::rollback();
     }
 
     protected function setUp(): void
