36c61713af6c2fb935ddd16b5e66433f1b7712cb
Simtel <simuls@gmail.com>
2024-08-20 16:23:56 +0400

    copy video to s3

--- app/Console/Commands/YotubeVideosDownload.php
+++ app/Console/Commands/YotubeVideosDownload.php
@@ -6,6 +6,8 @@ use Alaouy\Youtube\Facades\Youtube;
 use App\Models\YouTubeVideo;
 use Exception;
 use Illuminate\Console\Command;
+use Illuminate\Console\OutputStyle;
+use Illuminate\Support\Facades\Storage;
 use YoutubeDl\Options;
 use YoutubeDl\YoutubeDl;
 
@@ -40,7 +42,8 @@ class YotubeVideosDownload extends Command
                 continue;
             }
             $this->output->info('Обработка видео:' . $video->url);
-            $videoInfo = Youtube::getVideoInfo(Youtube::parseVidFromURL($video->url));
+            $videoId = Youtube::parseVidFromURL($video->url);
+            $videoInfo = Youtube::getVideoInfo($videoId);
             $video->title = $videoInfo->snippet->title;
             $this->output->info($video->title);
             $video->save();
@@ -49,7 +52,9 @@ class YotubeVideosDownload extends Command
 
             $progressBar = $this->output->createProgressBar();
             $progressBar->setBarWidth(50);
-            $progressBar->setFormat(' %current%/%max% [%bar%] %percent:3s%% %elapsed:6s%/%estimated:-6s% %memory:6s%');
+            $progressBar->setFormat(
+                ' %current%/%max% [%bar%] %percent:3s%% %elapsed:6s%/%estimated:-6s% %memory:6s%  %size% %speed%'
+            );
             $progressBar->start(100); // 100 - это % завершения, от 0 до 100
             $yt->onProgress(
                 function (
@@ -60,31 +65,54 @@ class YotubeVideosDownload extends Command
                     ?string $eta,
                     ?string $totalTime
                 ) use ($progressBar): void {
-                    // Преобразование процента в числовое значение
                     $percentNumber = (int)rtrim($percentage, '%');
-
-                    // Обновление прогресс-бара на основе процента
                     $progressBar->setProgress($percentNumber);
+                    $progressBar->setPlaceholderFormatter('size', function ($value) use ($size) {
+                        return $size;
+                    });
+                    $progressBar->setPlaceholderFormatter('speed', function ($value) use ($speed) {
+                        return $speed ?? '';
+                    });
                 }
             );
 
             $collection = $yt->download(
                 Options::create()
-                    ->downloadPath(public_path('videos'))
+                    ->downloadPath(Storage::disk('local')->path('public/videos'))
+                    ->output($videoId . '.%(ext)s')
                     ->url($video->url)
-                    ->format('mp4')
+                //->format('bestvideo[height<=1080]+bestaudio/best[height<=1080]')
             );
-
 
             foreach ($collection->getVideos() as $element) {
                 if ($element->getError() !== null) {
                     $this->output->error($element->getError());
                 } else {
+                    $filePath = 'public/videos' . '/' . $videoId . '.' . $element->getExt();
+                    $this->output->info($filePath);
+                    $this->copyFileToS3(
+                        $filePath,
+                        'videos/' . $element->getFilename(),
+                        $this->output
+                    );
                     $this->output->writeln('');
                     $this->output->writeln('');
                 }
             }
         }
         $this->output->success('Закончили скачивание');
+    }
+
+    public function copyFileToS3(string $localFilePath, string $s3FilePath, OutputStyle $output): void
+    {
+        if (Storage::disk('local')->exists($localFilePath)) {
+            $content = Storage::disk('local')->get($localFilePath);
+            if ($content !== null) {
+                Storage::disk('s3')->put($s3FilePath, $content);
+                return;
+            }
+        }
+
+        $output->error('Файл не найден!');
     }
 }
