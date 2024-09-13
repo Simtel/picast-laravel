532d0ab554b677e58cede1c904b58b3fd3dff040
Simtel <simuls@gmail.com>
2024-09-13 11:33:19 +0400

    fix status seeder

--- database/seeds/YouTubeVideoStatusSeeder.php
+++ database/seeds/YouTubeVideoStatusSeeder.php
@@ -2,35 +2,43 @@
 
 namespace Database\Seeders;
 
+use App\Models\Youtube\VideoStatus;
 use DB;
 use Illuminate\Database\Seeder;
 
 class YouTubeVideoStatusSeeder extends Seeder
 {
+    private array $statuses = [
+        'new'         => [
+            'title' => 'Новый',
+            'code'  => 'new',
+        ],
+        'queue'       => [
+            'title' => 'В очереди',
+            'code'  => 'queue',
+        ],
+        'downloading' => [
+            'title' => 'Загружается',
+            'code'  => 'downloading',
+        ],
+        'downloaded'  => [
+            'title' => 'Скачен',
+            'code'  => 'downloaded',
+        ]
+    ];
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        DB::table('youtube_video_statuses')->insert(
-            [
-                [
-                    'title' => 'Новый',
-                    'code'  => 'new',
-                ],
-                [
-                    'title' => 'В очереди',
-                    'code'  => 'queue',
-                ],
-                [
-                    'title' => 'Загружается',
-                    'code'  => 'downloading',
-                ],
-                [
-                    'title' => 'Скачен',
-                    'code'  => 'downloaded',
-                ]
-            ]
-        );
+        $rows = [];
+        $existsStatuses = VideoStatus::pluck('title', 'code')->all();
+        foreach ($this->statuses as $code => $status) {
+            if (!array_key_exists($code, $existsStatuses)) {
+                $rows[] = ['title' => $status['title'], 'code' => $status['code']];
+            }
+        }
+        DB::table('youtube_video_statuses')->insert($rows);
     }
 }
