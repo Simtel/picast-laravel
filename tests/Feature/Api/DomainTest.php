aebb704b993acec7ecf2e84aa56a02cb391e869d
Simtel <simuls@gmail.com>
2024-10-23 10:14:43 +0400

    add tests for api domains

--- tests/Feature/Api/DomainTest.php
+++ tests/Feature/Api/DomainTest.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace Tests\Feature\Api;
 
 use App\Context\Domains\Domain\Model\Domain;
+use App\Context\Domains\Infrastructure\Facades\Whois;
 use Event;
 use Illuminate\Testing\Fluent\AssertableJson;
+use Iodev\Whois\Modules\Tld\TldInfo;
+use Iodev\Whois\Modules\Tld\TldResponse;
 use Tests\TestCase;
 
 class DomainTest extends TestCase
@@ -42,14 +45,14 @@ class DomainTest extends TestCase
                 ->has(
                     'data.0',
                     fn (AssertableJson $json) => $json->where('id', $domain1->getId())
-                    ->where('name', $domain1->getName())
-                    ->etc()
+                        ->where('name', $domain1->getName())
+                        ->etc()
                 )
                 ->has(
                     'data.1',
                     fn (AssertableJson $json) => $json->where('id', $domain2->getId())
-                    ->where('name', $domain2->getName())
-                    ->etc()
+                        ->where('name', $domain2->getName())
+                        ->etc()
                 )
         );
     }
@@ -61,7 +64,10 @@ class DomainTest extends TestCase
         Event::fake();
         $domain = Domain::factory()->create(['user_id' => $user->getId()]);
 
-        $response = $this->get(route('api.domains.show', ['domain' => $domain]), ['Authorization' => 'Bearer ' . $user->api_token]);
+        $response = $this->get(
+            route('api.domains.show', ['domain' => $domain]),
+            ['Authorization' => 'Bearer ' . $user->api_token]
+        );
         $response->assertOk();
 
         $response->assertJson(
@@ -72,8 +78,8 @@ class DomainTest extends TestCase
                 ->has(
                     'data',
                     fn (AssertableJson $json) => $json->where('id', $domain->getId())
-                    ->where('name', $domain->getName())
-                    ->etc()
+                        ->where('name', $domain->getName())
+                        ->etc()
                 )
         );
     }
@@ -86,6 +92,7 @@ class DomainTest extends TestCase
         $response->assertStatus(403);
         $response->assertJson(fn (AssertableJson $json) => $json->where('message', 'Not action.'));
     }
+
     public function test_domain_edit_form(): void
     {
         $user = $this->getAdminUser();
@@ -93,8 +100,87 @@ class DomainTest extends TestCase
         Event::fake();
         $domain = Domain::factory()->create(['user_id' => $user->getId()]);
 
-        $response = $this->get(route('api.domains.edit', ['domain' => $domain]), ['Authorization' => 'Bearer ' . $user->api_token]);
+        $response = $this->get(
+            route('api.domains.edit', ['domain' => $domain]),
+            ['Authorization' => 'Bearer ' . $user->api_token]
+        );
         $response->assertStatus(403);
         $response->assertJson(fn (AssertableJson $json) => $json->where('message', 'Not action.'));
+    }
+
+    public function test_domain_store(): void
+    {
+        $user = $this->getAdminUser();
+
+        Event::fake();
+
+        $response = $this->post(
+            route('api.domains.store'),
+            ['name' => 'prosf.ru'],
+            ['Authorization' => 'Bearer ' . $user->api_token]
+        );
+        $response->assertOk();
+        $this->assertDatabaseCount(Domain::class, 1);
+        $this->assertDatabaseHas('domains', ['name' => 'prosf.ru']);
+    }
+
+    public function test_domain_update(): void
+    {
+        $user = $this->getAdminUser();
+        Event::fake();
+
+        $domain = Domain::factory()->create(['user_id' => $user->getId()]);
+
+        $info = self::createInfo($domain);
+        $info->expirationDate = now()->add('100 days')->getTimestamp();
+        $info->owner = 'Simtel';
+
+        Whois::expects('loadDomainInfo')
+            ->with($domain->name)
+            ->andReturn($info);
+
+
+        $response = $this->put(
+            route(
+                'api.domains.update',
+                ['domain' => $domain],
+            ),
+            ['domain' => $domain],
+            ['Authorization' => 'Bearer ' . $user->api_token]
+        );
+        $response->assertOk();
+        $this->assertDatabaseCount(Domain::class, 1);
+        $this->assertDatabaseHas('domains', ['name' => $domain->getName()]);
+        $this->assertDatabaseCount(\App\Context\Domains\Domain\Model\Whois::class, 1);
+        $this->assertDatabaseHas(\App\Context\Domains\Domain\Model\Whois::class, ['domain_id' => $domain->getId()]);
+    }
+
+    public function test_domain_delete(): void
+    {
+        $user = $this->getAdminUser();
+        Event::fake();
+
+        $domain = Domain::factory()->create(['user_id' => $user->getId()]);
+
+        $this->assertDatabaseCount(Domain::class, 1);
+
+        $response = $this->delete(route('api.domains.destroy', ['domain' => $domain]), [], ['Authorization' => 'Bearer ' . $user->api_token]);
+        $response->assertOk();
+
+        $this->assertDatabaseCount(Domain::class, 0);
+    }
+
+    private static function createInfo(Domain $domain): TldInfo
+    {
+        return new TldInfo(self::getResponse($domain));
+    }
+
+    private static function getResponse(Domain $domain): TldResponse
+    {
+        return new TldResponse([
+            "domain" => $domain->getName(),
+            "query" => $domain->getName(),
+            "text" => "Hello world",
+        ]);
     }
 }
