9300dce53f20bc37d209479d0280cf3d58cb81bb
Simtel <simuls@gmail.com>
2024-11-27 15:00:18 +0400

    add test to notify create domain

--- tests/Unit/Context/Domains/DomainNotifyTest.php
+++ tests/Unit/Context/Domains/DomainNotifyTest.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace Tests\Unit\Context\Domains;
 
 use App\Context\Domains\Domain\Model\Domain;
+use App\Context\Domains\Infrastructure\Notification\DomainCreated;
 use App\Context\Domains\Infrastructure\Notification\DomainDeleted;
 use Event;
 use Tests\TestCase;
 
-class DomainDeletedNotifyTest extends TestCase
+class DomainNotifyTest extends TestCase
 {
     public function test_domain_deleted_notify(): void
     {
@@ -22,5 +23,17 @@ class DomainDeletedNotifyTest extends TestCase
         self::assertContains($domain->getName() . ' был удален из системы.', $mail->introLines);
         $telegram = $notify->toTelegram(new \stdClass());
         self::assertEquals('Домен ' . $domain->getName() . ' был удален из системы.', $telegram->getMessage());
+    }
+
+
+    public function test_domain_created_notify(): void
+    {
+        Event::fake();
+        $domain = Domain::factory()->create();
+
+        $notify = new DomainCreated($domain);
+        self::assertEquals([], $notify->toArray(new \stdClass()));
+        $mail = $notify->toMail(new \stdClass());
+        self::assertContains($domain->getName() . ' был добавлен в систему.', $mail->introLines);
     }
 }
