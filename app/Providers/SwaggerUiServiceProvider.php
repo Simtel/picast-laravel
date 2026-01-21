851f3f0e1c52823fe2ca614654412a7cf05d7b64
Simtel <simuls@gmail.com>
2026-01-21 09:57:21 +0400

    fix cs

--- app/Providers/SwaggerUiServiceProvider.php
+++ app/Providers/SwaggerUiServiceProvider.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
@@ -7,9 +9,9 @@ use Illuminate\Support\ServiceProvider;
 
 class SwaggerUiServiceProvider extends ServiceProvider
 {
-    public function boot() : void
+    public function boot(): void
     {
-        Gate::define('viewSwaggerUI', function ($user = null) {
+        Gate::define('viewSwaggerUI', static function ($user = null) {
             return in_array(optional($user)->email, [
                 //
             ]);
